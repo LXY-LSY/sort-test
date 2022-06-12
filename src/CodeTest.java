47bf74df3fb65aac4d7321eb1a373d263a24f07b
Lxy <1016516143@qq.com>
2022-06-12 16:27:34 +0800

    second commit

--- src/CodeTest.java
+++ src/CodeTest.java
@@ -299,7 +299,7 @@ public class CodeTest {
 
     public static void main(String[] args) {
         int[] arr = new int[]{9,8,7,5,6,3,4,2,1,0};
-
+        System.out.println("jaja");
         bucketSort(arr);
         System.out.println(Arrays.toString(arr));
     }
